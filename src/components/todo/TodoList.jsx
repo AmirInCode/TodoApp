e7c8cc9a4d9f9fd3fd5525dd58a311997c5b195e
amir mmd <amiresrari@gmail.com>
2026-06-01 16:31:56 +0330

    update project

--- src/components/todo/TodoList.jsx
+++ src/components/todo/TodoList.jsx
@@ -1,17 +1,10 @@
-import { ListTodo } from "lucide-react";
-
 import TodoItem from "./TodoItem";
+import EmptyState from "./EmptyState";
 
 const TodoList = ({ todos, onDelete, onUpdate,onToggle }) => {
   if (todos.length === 0) {
     return (
-      <div className="flex flex-col items-center gap-3 mt-20">
-        <ListTodo className="size-12 text-gray-500/50" />
-
-        <p className="text-center text-gray-500">
-          No tasks yet. Add one above!
-        </p>
-      </div>
+      <EmptyState />
     );
   }
 
